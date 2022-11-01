c6f03f0d72f4bca7c3974e209b3f38814856546a
alexpandy <alexpandyvijay@gmail.com>
2022-11-01 16:13:38 +0530

    first lanch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value , setValue] = useState('');
+  const onClickHandler = (e) => {
+    setValue(value+e);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <h1>Calculator</h1>
+        <div className="input">
+            <div className="display">{value}</div>
+            <div className="c">C</div>
+        </div>
+        <div className="num">
+            <div onClick={()=>(onClickHandler(1))}>1</div>
+            <div onClick={()=>(onClickHandler(2))}>2</div>
+            <div onClick={()=>(onClickHandler(3))}>3</div>
+            <div onClick={()=>(onClickHandler('/'))}>/</div>
+            <div onClick={()=>(onClickHandler(4))}>4</div>
+            <div onClick={()=>(onClickHandler(5))}>5</div>
+            <div onClick={()=>(onClickHandler(6))}>6</div>
+            <div onClick={()=>(onClickHandler('-'))}>-</div>
+            <div onClick={()=>(onClickHandler(7))}>7</div>
+            <div onClick={()=>(onClickHandler(8))}>8</div>
+            <div onClick={()=>(onClickHandler(9))}>9</div>
+            <div onClick={()=>(onClickHandler('+'))}>+</div>
+            <div onClick={()=>(onClickHandler('.'))}>.</div>
+            <div onClick={()=>(onClickHandler(0))}>0</div>
+            <div >=</div>
+            <div onClick={()=>(onClickHandler('*'))}>*</div>
+        </div>
+      </div>
     </div>
   );
 }
